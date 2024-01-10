61997e5413d87cfeaa59c359f42021ce8bdc3f31
Omar <saucyfunnel@gmail.com>
2024-01-10 15:22:09 -0500

    tailwind config con chadcn

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+const MaxWidthWrapper = ({
+    className,
+    children
+}:{
+    className?: string
+    children: ReactNode
+}) => {
+    return(
+        <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+            {children}
+        </div>
+    )
+}
