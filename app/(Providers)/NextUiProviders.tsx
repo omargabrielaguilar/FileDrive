6c2551d509d95066a09b128b1667d70f6008242b
Omar <saucyfunnel@gmail.com>
2024-01-11 16:13:26 -0500

    (providers) y (main)

---
+++ app/(Providers)/NextUiProviders.tsx
@@ -0,0 +1,11 @@
+'use client'
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+
+interface ProviderProps {
+  children: React.ReactNode;
+}
+
+export default function Provider({ children }: ProviderProps) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
