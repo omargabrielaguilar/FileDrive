a367b9419a4135fca4bd494951b2554fb2e58445
Omar <saucyfunnel@gmail.com>
2024-02-29 15:53:50 -0500

    initialize

--- src/posts/posts.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostsService } from './posts.service';
-import { PostsResolver } from './posts.resolver';
-
-@Module({
-  providers: [PostsService, PostsResolver],
-})
-export class PostsModule {}
