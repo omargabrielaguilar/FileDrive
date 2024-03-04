41ac75facc4b2f27bdfa5cb51420076c2862412c
Omar <saucyfunnel@gmail.com>
2024-03-04 12:01:01 -0500

    query identity

--- convex/files.ts
+++ convex/files.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const createFile = mutation({
@@ -6,6 +6,12 @@ export const createFile = mutation({
     name: v.string(),
   },
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    
+    if (!identity) {
+      throw new ConvexError('debes estar logeado para subir un archivo')
+    }
+
     await ctx.db.insert("files", {
       name: args.name,
     });
@@ -15,6 +21,12 @@ export const createFile = mutation({
 export const getFiles = query({
   args: {},
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if(!identity) {
+      return [];
+    }
+
     return ctx.db.query("files").collect();
   },
 });
